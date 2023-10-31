4788fb8879cd64579d40842fb4e7886052bc45a9
milanpanyik <milanpanyik@gmail.com>
2023-10-31 14:25:08 +0100

    created folders and files

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>Jammming</h1>
+            <div name="searchbar">
+                <div>
+                    <p>asd</p>
+                    <p>asd</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default App;
