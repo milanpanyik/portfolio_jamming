917c35b3acdd5700adfef8a1d295a36d742f59dc
milanpanyik <milanpanyik@gmail.com>
2023-11-10 11:33:40 +0100

    different approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const searchSong = async () => {
+  const searchTerm = "eminem";
+  const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + searchTerm;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c5a03594c9mshb2b4b18058c0a81p1bbb3ejsn4f6cb597418e",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result.data[0]);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 function App() {
+  //searchSong();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Body*/}
+      <div>
+        <h1>Jammming</h1>
+      </div>
+      <div>
+        <input placeholder="text here" />
+        <button>Search </button>
+      </div>
+      <div className="columns">
+        <div>
+          <p>a</p>
+        </div>
+        <div>
+          <input placeholder="playlist name "></input>
+        </div>
+      </div>
     </div>
   );
 }
